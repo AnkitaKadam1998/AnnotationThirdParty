58bc70a718af9165d4a86816107cc94d88d73b27
komal@komal405.com <ankita80kadam@gmail.com>
2021-05-15 12:50:33 +0530

    ADD:Read data addressbook as csv file

--- AddressBookReadData.java
+++ AddressBookReadData.java
@@ -1,7 +1,33 @@
-class AddressBookReadData
-{
-public static void main(String args[])
-{
-System.out.println("Welcome");
+package briglabz.Watch_Service;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class AddressBookReadData {
+	
+	public static void main(String args[])
+	{
+		String path = "C:\\Users\\Ankita\\eclipse-workspace\\Watch-Service\\Data\\user.csv";
+		String line= " ";
+		try 
+		{
+			BufferedReader br= new  BufferedReader(new FileReader(path));
+			
+			while((line = br.readLine()) != null)
+			{
+				System.out.println("User data:"+line);
+			}
+		} 
+		catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+        }
+	}
 }
-}
+
